af75c8001917653f179273945da245d92c193ead
QuixDevf <zunnur.brad@gmail.com>
2018-01-04 21:42:46 +0800

    Changes to Fuel Price interface

---
+++ src/pages/fuel_prices/fuel_prices.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-fuel_prices',
+  templateUrl: 'fuel_prices.html'
+})
+export class FuelPricesPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
